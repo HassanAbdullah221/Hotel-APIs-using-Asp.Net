4b5dfeba590421d887c52e998c8845cf50049861
Hassan Abdullah <hassan8abdulla@gmail.com>
2026-05-04 12:54:34 +0300

    added countries controller

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,89 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrainingProject.Data;
+
+namespace HotelListing.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CountriesController(HotelListingDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+    {
+        var countries = await context.Countries.ToListAsync();
+        return countries;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Country>> GetCountry(int id)
+    {
+        var country = await context.Countries.FindAsync(id);
+
+        if (country == null)
+        {
+            return NotFound();
+        }
+
+        return country;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCountry(int id, Country country)
+    {
+        if (id != country.CountryId)
+        {
+            return BadRequest();
+        }
+
+        context.Entry(country).State = EntityState.Modified;
+
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!await CountryExistsAsync(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Country>> PostCountry(Country country)
+    {
+        context.Countries.Add(country);
+        await context.SaveChangesAsync();
+
+        return CreatedAtAction("GetCountry", new { id = country.CountryId }, country);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCountry(int id)
+    {
+        var country = await context.Countries.FindAsync(id);
+        if (country == null)
+        {
+            return NotFound();
+        }
+
+        context.Countries.Remove(country);
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private async Task<bool> CountryExistsAsync(int id)
+    {
+        return await context.Countries.AnyAsync(e => e.CountryId == id);
+    }
+}
--- Data/Country.cs
+++ Data/Country.cs
@@ -3,8 +3,8 @@
     public class Country
     {
         public int CountryId { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string ShortName { get; set; } = string.Empty;
-        public IList<Hotel> Hotels { get; set; }
+        public string Name { get; set; } 
+        public string ShortName { get; set; } 
+        public IList<Hotel> Hotels { get; set; } = [];
     }
 }
